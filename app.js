174d1b917911280b9919dca51d67277e4ce40e1d
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-24 14:26:16 +0200

    Design 3

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const buttonShare = document.getElementById("button-share");
-const userCardSection = document.querySelector('.user-card-section')
+const userCardSection = document.querySelector(".user-card-section");
+
+function mobileButtonShareHandler() {
+    if (userCardSection.style.backgroundColor !== "var(--very-dark-grayish-blue)") {
+        userCardSection.style.backgroundColor = "var(--very-dark-grayish-blue)";
+      } else {
+        userCardSection.style.backgroundColor = "white";
+      }
+}
+
+function desktopButtonShareHandler() {
+    if (userCardSection.style.backgroundColor !== "var(--very-dark-grayish-blue)") {
+        userCardSection.style.backgroundColor = "var(--very-dark-grayish-blue)";
+      } else {
+        userCardSection.style.backgroundColor = "white";
+      }
+}
+
 
 function buttonShareHandler() {
-  
-    (userCardSection.style.backgroundColor === 'var(--very-dark-grayish-blue)') ?
-        userCardSection.style.backgroundColor = 'white'
-        :
-        userCardSection.style.backgroundColor = 'var(--very-dark-grayish-blue)';
+    if (window.innerWidth < 1000) {
+        mobileButtonShareHandler();
+      } else {
+        desktopButtonShareHandler();
+      }
 }
 
 buttonShare.addEventListener("click", buttonShareHandler);
