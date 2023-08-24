970bdcb103572a5a47efcce351045814303312d8
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-24 13:01:39 +0200

    Design 1

---
+++ app.js
@@ -0,0 +1,7 @@
+const buttonShare = document.getElementById('button-share')
+
+function buttonShareHandler() {
+    console.log ('click')
+}
+
+buttonShare.addEventListener ('click', buttonShareHandler)
