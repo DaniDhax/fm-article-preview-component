951d0cb9697f7db2e45905941bcf2e64c47b9a49
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-25 11:02:29 +0200

    Design 4

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const buttonShare = document.getElementById("button-share");
 const userCardSection = document.querySelector(".user-card-section");
-
+const colorSvg = document.querySelector('#icon-share-svg path');
+const userDiv = document.querySelector('.user');
+const mobileSocialMediaDiv = document.querySelector('.mobile-social-media-div')
+ 
 function mobileButtonShareHandler() {
     if (userCardSection.style.backgroundColor !== "var(--very-dark-grayish-blue)") {
         userCardSection.style.backgroundColor = "var(--very-dark-grayish-blue)";
+        userDiv.style.display = 'none'
+        mobileSocialMediaDiv.style.display = 'flex'
       } else {
         userCardSection.style.backgroundColor = "white";
+        userDiv.style.display = 'flex'
+        mobileSocialMediaDiv.style.display = 'none'
       }
 }
 
 function desktopButtonShareHandler() {
-    if (userCardSection.style.backgroundColor !== "var(--very-dark-grayish-blue)") {
-        userCardSection.style.backgroundColor = "var(--very-dark-grayish-blue)";
+    if (colorSvg.getAttribute('fill') === '#6E8098') {
+        colorSvg.setAttribute('fill', 'white');
+        buttonShare.style.backgroundColor = 'var(--very-dark-grayish-blue)';
       } else {
-        userCardSection.style.backgroundColor = "white";
+        colorSvg.setAttribute('fill', '#6E8098');
+        buttonShare.style.backgroundColor = 'var(--light-grayish-blue)';
       }
 }
 
