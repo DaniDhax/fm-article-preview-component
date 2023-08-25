b69e76fc005d266ad0ceff27ef236cc1b8794fa6
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-25 11:33:20 +0200

    Design 5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const userCardSection = document.querySelector(".user-card-section");
 const colorSvg = document.querySelector('#icon-share-svg path');
 const userDiv = document.querySelector('.user');
 const mobileSocialMediaDiv = document.querySelector('.mobile-social-media-div')
- 
+const desktopSocialMediaDiv = document.querySelector('.desktop-social-media-div') 
+
 function mobileButtonShareHandler() {
     if (userCardSection.style.backgroundColor !== "var(--very-dark-grayish-blue)") {
         userCardSection.style.backgroundColor = "var(--very-dark-grayish-blue)";
@@ -20,12 +21,13 @@ function desktopButtonShareHandler() {
     if (colorSvg.getAttribute('fill') === '#6E8098') {
         colorSvg.setAttribute('fill', 'white');
         buttonShare.style.backgroundColor = 'var(--very-dark-grayish-blue)';
+        desktopSocialMediaDiv.style.display = 'flex'
       } else {
         colorSvg.setAttribute('fill', '#6E8098');
         buttonShare.style.backgroundColor = 'var(--light-grayish-blue)';
+        desktopSocialMediaDiv.style.display = 'none'
       }
 }
-
 
 function buttonShareHandler() {
     if (window.innerWidth < 1000) {
